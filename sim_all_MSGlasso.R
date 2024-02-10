3798beaf1035ef871d8270c16cfcb5343f1beb47
huj156 <huj156@hotmail.com>
2024-02-10 23:06:22 +0800

    Add files via upload

---
+++ sim_all_MSGlasso.R
@@ -0,0 +1,43 @@
+library(MSGLasso)
+library(R.matlab)
+#setwd("/Users/john/Library/CloudStorage/Dropbox/huj/Fujikoshi/model_selection/my_version/Sinica/revision/simulation/simall-20240130/")
+setwd("C:/Users/admin/OneDrive/simall-20240130/")
+Data <- readMat("modelXY.mat")
+file.remove("modelXY.mat")
+Lam1G <- readMat("lam1G.mat")
+X.m<-Data$X
+Y.m<-Data$Y
+
+P <- dim(X.m)[2]
+Q <- dim(Y.m)[2]
+G <- P
+R <- 1
+
+gmax <- 1
+cmax <- Q*P
+GarrStarts <- seq(from=0,to=P-1,by=1)
+GarrEnds <- seq(from=0,to=P-1,by=1)
+RarrStarts <-c(0)
+RarrEnds <- c(Q-1)
+
+tmp <- FindingPQGrps(P, Q, G, R, gmax, GarrStarts, GarrEnds, RarrStarts, RarrEnds)
+PQgrps <- tmp$PQgrps
+
+tmp1 <- Cal_grpWTs(P, Q, G, R, gmax, PQgrps)
+grpWTs <- tmp1$grpWTs
+
+tmp2 <- FindingGRGrps(P, Q, G, R, cmax, GarrStarts, GarrEnds, RarrStarts, RarrEnds)
+GRgrps <- tmp2$GRgrps
+
+Pen_L <- matrix(rep(1,P*Q),P,Q, byrow=TRUE)
+Pen_G <- matrix(rep(1,G*R),G,R, byrow=TRUE)
+grp_Norm0 <- matrix(rep(0, G*R), nrow=G, byrow=TRUE)
+
+MSGLassolam1 <- Lam1G$lam1G[1]
+MSGLassolamG <- Lam1G$lam1G[2]
+
+MSGLassolamG.m <- matrix(rep(MSGLassolamG, G*R),G,R,byrow=TRUE)
+
+system.time(try <-MSGLasso(X.m, Y.m, grpWTs, Pen_L, Pen_G, PQgrps, GRgrps, grp_Norm0,
+                           MSGLassolam1, MSGLassolamG.m, Beta0=NULL))
+writeMat("hatTheta.mat", hattheta=try$Beta)
